7ca0cb7c6ae8c477d41a3c2910bb7f61c5911feb
HanselHu <apologize0616@gmail.com>
2016-03-15 15:30:57 +0800

    作業批改完成!

--- HwTest.java
+++ HwTest.java
@@ -1,32 +1,36 @@
+/*
+ * Ok, good code!
+ * */
+
 import java.util.Scanner;
 
 public class HwTest {
-	public static void main(String []args){
+	public static void main(String[] args) {
 		double num1, num2;
-        Scanner scanner = new Scanner(System.in);
-		
+		Scanner scanner = new Scanner(System.in);
+
 		System.out.println("insert one number A:");
 		num1 = scanner.nextDouble();
 		System.out.println("insert another number B:");
 		num2 = scanner.nextDouble();
-		
+
 		double A = num1 + num2;
 		double B = num1 - num2;
 		double C = num1 * num2;
 		System.out.println("A+B=" + A);
 		System.out.println("A-B=" + B);
 		System.out.println("A*B=" + C);
-		
-		double D = num1 / num2;
-		if(num2 == 0){
+
+		double D = num1 / num2;// 是否先做驗證在進行運算會比較安全呢?
+		if (num2 == 0) {
 			System.out.println("A/B is invalid");
 		}
-		
-		else{
+
+		else {
+			// double D = num1 / num2;
 			System.out.println("A/B=" + D);
 		}
-		
-		
+
 	}
 
 }
--- RectangleTest.java
+++ RectangleTest.java
@@ -1,53 +1,58 @@
-class Rectangle{
+/*
+ * [A]100
+ * [TA's advise]
+ * 1.寫的很好也感覺得出你的細心, 強制轉型的概念很好, 不過當你已經預期的到method的回傳的資料型態, 那此時強制轉型是否會有所多於呢?
+ * 2.Good Job!
+ * */
+
+class Rectangle {
 	double x, y, width, height;
-	
-	Rectangle(){
-		
+
+	Rectangle() {
+		// this(-1,-1,-1,-1);//加上這一行會讓你程式更好.
 	}
-	
-	Rectangle(double _x, double _y, double _width, double _height){
+
+	Rectangle(double _x, double _y, double _width, double _height) {
 		this.x = _x;
-	    this.y = _y;
-	    this.width = _width;
-	    this.height = _height;
+		this.y = _y;
+		this.width = _width;
+		this.height = _height;
 	}
-	
-	double getWidth(){
+
+	double getWidth() {
 		return this.width;
 	}
-	
-	double getHeight(){
+
+	double getHeight() {
 		return this.height;
 	}
-	
-	public String toString(){
+
+	public String toString() {
 		return "java.Rectangle[x=" + this.x + ",y=" + this.y + ",width=" + this.width + ",height=" + this.height + "]";
 	}
-	
-	
+
 }
 
 public class RectangleTest {
-	public static void main(String []args){
-		Rectangle rectangle1 = new Rectangle (15, 25, 57, 18);
-		Rectangle rectangle2 = new Rectangle (0, 12, 60, 47);
-		double width1 = (double)rectangle1.getWidth();
-		double height1 = (double)rectangle1.getHeight();		
-		
-		double width2 = (double)rectangle2.getWidth();
-		double height2 = (double)rectangle2.getHeight();
-		
+	public static void main(String[] args) {
+		Rectangle rectangle1 = new Rectangle(15, 25, 57, 18);
+		Rectangle rectangle2 = new Rectangle(0, 12, 60, 47);
+		double width1 = (double) rectangle1.getWidth();
+		double height1 = (double) rectangle1.getHeight();
+
+		double width2 = (double) rectangle2.getWidth();
+		double height2 = (double) rectangle2.getHeight();
+
 		System.out.println(rectangle1.toString());
 		System.out.println("Area = " + width1 * height1);
 		System.out.println("Perimeter = " + (width1 + height1) * 2);
 		System.out.println();
 		System.out.println(rectangle2.toString());
-	    System.out.println("Area = " + width2 * height2);
+		System.out.println("Area = " + width2 * height2);
 		System.out.println("Perimeter = " + (width2 + height2) * 2);
 		System.out.println();
 		System.out.println("End of Output!");
-		
-		
+
 	}
 
 }
